6ce2a9ab1c20fdb9114215140a83def1b43aed7b
Roberto Torres <roberto.torresdn@gmail.com>
2013-09-11 01:35:02 -0300

    Second Commit

---
+++ Column.py
@@ -0,0 +1,31 @@
+class Column:
+
+	def setStrFieldColumn(self, strFieldColumn):
+		self.strFieldColumn = strFieldColumn
+
+	def getStrFieldColumn(self):
+		return self.strFieldColumn
+
+	def setStrTypeColumn(self, strTypeColumn):
+		self.strTypeColumn = strTypeColumn
+
+	def getStrTypeColumn(self):
+		return self.strTypeColumn
+
+	def setStrKeyColumn(self, strKeyColumn):
+		self.strKeyColumn = strKeyColumn
+
+	def getStrKeyColumn(self):
+		return self.strKeyColumn
+
+	def setStrExtraColumn(self, strExtraColumn):
+		self.strExtraColumn = strExtraColumn
+
+	def getStrExtraColumn(self):
+		return self.strExtraColumn
+
+	def setStrNameTable(self, strNameTable):
+		self.strNameTable = strNameTable
+
+	def getStrNameTable(self):
+		return self.strNameTable
---
+++ DataBase.py
@@ -0,0 +1,7 @@
+class DataBase:
+
+	def setStrNombreBaseDatos(self, strNombreBaseDatos):
+		self.strNombreBaseDatos = strNombreBaseDatos
+
+	def getStrNombreBaseDatos(self):
+		return self.strNombreBaseDatos
