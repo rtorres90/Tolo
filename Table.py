0e1ee9337d173191524181ac94c03fc4992aff9d
Roberto Torres <roberto.torresdn@gmail.com>
2013-09-11 01:40:27 -0300

    Third Commit

---
+++ Table.py
@@ -0,0 +1,13 @@
+class Table:
+
+	def setStrNameTable(self, strNameTable):
+		self.strNameTable = strNameTable
+
+	def getStrNameTable(self):
+		return self.strNombreTable
+
+	def setStrNameDataBase(self, strNameDataBase):
+		self.strNameDataBase = strNameDataBase
+
+	def getStrNameDataBase(self):
+		return self.strNameDataBase
